7088bb68ac55bce9ca84fe919bc5c23340613632
Isaac Cook <isaac@simpload.com>
2015-07-05 17:12:40 -0500

    Adds voting arrows to the post view page as well

---
+++ static/js/vote_script.js
@@ -0,0 +1,14 @@
+$(function () {
+  $(".vote").click(function (e) {
+    var sel = $(this);
+    var obj = sel[0];
+    sel.toggleClass('enabled');
+    if (obj.dataset.dir == "up")
+      sel.siblings(".down").removeClass('enabled');
+    else
+      sel.siblings(".up").removeClass('enabled');
+    $.ajax({
+      url: "/api/vote/" + obj.dataset.id + "/" + obj.dataset.dir,
+    });
+  });
+});
