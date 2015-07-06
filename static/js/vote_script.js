e8dbbf44dd8ccf98b2e60663db962ba39640059d
Isaac Cook <isaac@simpload.com>
2015-07-06 13:06:54 -0500

    Update the vote count display when voting

--- static/js/vote_script.js
+++ static/js/vote_script.js
@@ -2,13 +2,32 @@ $(function () {
   $(".vote").click(function (e) {
     var sel = $(this);
     var obj = sel[0];
-    sel.toggleClass('enabled');
-    if (obj.dataset.dir == "up")
-      sel.siblings(".down").removeClass('enabled');
-    else
-      sel.siblings(".up").removeClass('enabled');
+    var direction = obj.dataset.dir;
+    var up = obj.dataset.dir == "up";
+    var change = 0;
+    if (sel.toggleClass('enabled').hasClass('enabled')) {
+        // Only run if we aren't removing the vote
+        change += up ? 1 : -1;
+        if (up) {
+            if (sel.siblings(".down").hasClass('enabled')) {
+                sel.siblings(".down").removeClass('enabled');
+                change += 1;
+            }
+        } else {
+            if (sel.siblings(".up").hasClass('enabled')) {
+                sel.siblings(".up").removeClass('enabled');
+                change -= 1;
+            }
+        }
+    } else {
+        change += up ? -1 : 1;
+        direction = "remove";
+    }
+    display_score = parseInt(sel.siblings('.score').html());
+    display_score += change;
+    sel.siblings('.score').html(display_score);
     $.ajax({
-      url: "/api/vote/" + obj.dataset.type + "/" + obj.dataset.group + "/" + obj.dataset.id + "/" + obj.dataset.dir,
+      url: "/api/vote/" + obj.dataset.type + "/" + obj.dataset.group + "/" + obj.dataset.id + "/" + direction,
     });
   });
 });
