c486823c34342bf5e9dc53fdb192346f605b41a6
Eric Cook <eric@simpload.com>
2015-07-08 11:30:14 -0500

    Reload on submit Cleanup js spacing

--- static/js/comment.js
+++ static/js/comment.js
@@ -4,22 +4,23 @@ $(function () {
     var data = sel.serializeObject();
     data['post_id'] = post_id;
     $.ajax({
-        method: "POST",
-        dataType: "json",
-        url: "/api/post_comment/" + obj.dataset.parent,
-        data: data
+      method: "POST",
+      dataType: "json",
+      url: "/api/post_comment/" + obj.dataset.parent,
+      data: data
     });
-  }
+  };
   $("[name=submit]").click(function (e) {
-      submission($(this).parents('form'));
+    submission($(this).parents('form'));
+    location.reload();
   });
   $(".reply-link").click(function (e) {
-      var t = $(this);
-      var div = t.parent().children('.comment-reply');
-      var form = $("#comment-form").clone().attr("data-parent", this.dataset.parent);
-      form.find('[name=submit]').click(function (e) {
-          submission($(this).parents('form'));
-      });
-      div.html(form);
+    var t = $(this);
+    var div = t.parent().children('.comment-reply');
+    var form = $("#comment-form").clone().attr("data-parent", this.dataset.parent);
+    form.find('[name=submit]').click(function (e) {
+      submission($(this).parents('form'));
+    });
+    div.html(form);
   });
 });
